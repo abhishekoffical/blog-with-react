130adb098d6d9ecc4eaef7463082cf2b817c1224
abhishek yadav <ay6392915727@gmail.com>
2026-07-15 14:59:45 +0530

    project and seprate auth setup

---
+++ src/conf/config.js
@@ -0,0 +1,12 @@
+const conf= {
+    appwriteURL:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseCollection:String(import.meta.env.VITE_APPWRITE_DATABASE_COLLECTION)
+,
+    appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+
+}
+
+
+export default conf
