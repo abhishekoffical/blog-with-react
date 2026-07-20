9bb7a78c137373016a025bd302b817a75c0bc2e8
abhishek yadav <ay6392915727@gmail.com>
2026-07-20 20:26:18 +0530

    bug fix of header

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -2,40 +2,42 @@ import React from 'react'
 import {useNavigate} from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import Container from '../Components/Container'
-import {logout} from '../store/authSlice'
+// import {logout} from '../store/authSlice'
 import Logo from '../Components/Logo'
 import { useSelector } from 'react-redux'
-import { addPosts } from '../store/postSlice'
-import {LogoutBtn} from './LogoutBtn'
+// import { addPosts } from '../store/postSlice'
+import LogoutBtn from './LogoutBtn'
 
 function Header() {
   const Navigate=useNavigate()
-  const Authcheck= useSelector((state)=>{state.auth.status})
+  const Authcheck= useSelector((state)=>state.auth.status
+    
+  )
   const NavItems=[{
-    name:Home ,
+    name:"Home" ,
     slug :"/",
     active:true
   },{
-    name:Login ,
+    name:"Login" ,
     slug:"/login",
     active:!Authcheck
   },
 {
-  name:Signup,
+  name:"Signup",
   slug:"/signup",
   active:!Authcheck
 },{
-  name:Allpost,
-  slug:"all-post",
+  name:"Allpost",
+  slug:"/allpost",
   active:Authcheck
 },{
-  name:addPosts,
+  name:"addPosts",
   slug: "/addPosts",
   active:Authcheck
 }]
   return (
    <header className='shadow py-3 bg-gray-500'>
-    <Container >
+    <Container  >
       <nav className='flex'>
         <div className='mr-4'>
           <Link to='/'>
@@ -45,7 +47,7 @@ function Header() {
         <ul className='flex ml-auto'>
           {NavItems.map((item)=>(
              item.active? <li key={item.name}>
-             <button className='inline-bock px-6 py-2 duration-100 hover:bg-blue-100 rounded-full' onClick={Navigate(item.slug)}>{item.name}</button>
+             <button className='inline-bock text-amber-700 px-6 py-2 duration-100 hover:bg-blue-100 rounded-full' onClick={()=>{Navigate(item.slug)}}>{item.name}</button>
 
              </li>:null
           ))}
